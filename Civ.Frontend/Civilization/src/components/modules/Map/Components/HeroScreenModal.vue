bbf742cc500e2973de6b90d5d6272d2e80b0486b
LAPTOP-BIR7OQ53\warwe <warwelong@gmail.com>
2023-10-23 02:07:13 +0200

    designed hero modal scaffolding

--- Civ.Frontend/Civilization/src/components/modules/Map/Components/HeroScreenModal.vue
+++ Civ.Frontend/Civilization/src/components/modules/Map/Components/HeroScreenModal.vue
@@ -1,7 +1,18 @@
 <template>
     <Modal :modalActive="open">
-        <div>
-
+        <div id="overall">
+            <div id="stats">
+                <h3>Stats</h3>
+            </div>
+            <div id="abilities">
+                <h3>Abilities</h3>
+            </div>
+            <div id="inventory">
+                <h3>Inventory</h3>
+            </div>
+            <div id="team">
+                <h3>Team</h3>
+            </div>
         </div>
         <div>
             <button @click="closeModal">Close</button>
@@ -16,6 +27,36 @@
         width: 50px;
         height: 30px;
         margin-left: 5px;
+    }
+
+    #overall div{
+        border: 2px red solid;
+        margin: 1%;
+        padding: 5px
+    }
+
+    #stats{
+        float: left;
+        width: 38%;
+
+    }
+
+    #abilities{
+        float: right;
+        width: 58%;
+
+    }
+
+    #inventory{
+        float: left;
+        width: 38%;
+
+    }
+
+    #team{
+        float: right;
+        width: 58%; 
+
     }
 </style>
 
